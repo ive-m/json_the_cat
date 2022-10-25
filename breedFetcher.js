cd44dfd403936acfa2aa19468bd4f5037a94e79d
ive-m <ivesita.maria@gmail.com>
2022-10-24 22:30:32 -0400

    Cat Breed Fetcher

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const arg = process.argv;
+
+
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${arg[2]}`, (error, response, body)=>{
+  if (error) {
+    console.log("ERROR", error);
+  } else {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log("Breed not found");
+    } else {
+      console.log(data[0].description);
+    }
+ 
+  }
+ 
+});
+  
+
